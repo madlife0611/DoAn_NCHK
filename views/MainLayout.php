25a631afaa07ac34e6db59450184e7e5fd84b984
madlife0611 <madlife06112001@gmail.com>
2023-03-30 01:21:26 +0700

    30/03
    add adminlte3

--- views/MainLayout.php
+++ views/MainLayout.php
@@ -6,21 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- add css chung-->
-    <link rel="stylesheet" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" href="../assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
 
     <!-- add css cho phan nhan vien -->
-    <link rel="stylesheet" href="../assets/user/styles.css">
+    <link rel="stylesheet" href="assets/users/css/styles.css">
     <!-- add js chung -->
-    <script src="../assets/js/bootstrap.bundle.js"></script>
-    <script src="../assets/js/jquery-3.6.3.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/jquery-3.6.3.min.js"></script>
 </head>
 <body>
     <div class="cover"></div>
+    
     <div class="wrapper">
         <!-- header -->
         <div class="header">
-
+            <i class="fa-solid fa-magnifying-glass"></i>
         </div>
         <!-- end header -->
         <!-- Main content -->
